235be8b410be8a84d3733c527956bacb58ba29fb
Petar Zec <63071277+PetarZec01@users.noreply.github.com>
2022-12-06 15:50:30 +0100

    Day 6 advent of code solution

---
+++ AdventOfCode/Days/Day6.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+   
+    public class Day6
+    {
+        public int findMarker(string subroutine) 
+        {
+            bool foundOneRepetition = true;
+            for (int i = 0; i < subroutine.Length - 4; i++)
+            {
+                foundOneRepetition = false;
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        if (subroutine[j+i] == subroutine[k+i] && j!=k)
+                        {
+                            foundOneRepetition = true;
+                        }
+                    }
+                }
+                if (foundOneRepetition == false) return i + 4;
+            }
+            return 0;
+        }
+
+        public int findMessage(string subroutine)
+        {
+            bool foundOneRepetition = true;
+            for (int i = 0; i < subroutine.Length - 14; i++)
+            {
+                foundOneRepetition = false;
+                for (int j = 0; j < 14; j++)
+                {
+                    for (int k = 0; k < 14; k++)
+                    {
+                        if (subroutine[j + i] == subroutine[k + i] && j != k)
+                        {
+                            foundOneRepetition = true;
+                        }
+                    }
+                }
+                if (foundOneRepetition == false) return i + 14;
+            }
+            return 0;
+        }
+        public void Solution()
+        {
+            Console.WriteLine("Day 6:");
+            LinkedList<string> list = FileReader.ReadFile("C:\\Users\\petar\\Desktop\\advent of code\\AdventOfCode\\inputs\\Day6.txt");
+
+            string subroutine = list.First();
+            Console.WriteLine(findMarker(subroutine));
+            Console.WriteLine(findMessage(subroutine));
+
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,6 +11,7 @@ namespace AdventOfCode
             Day3 day3 = new Day3(); day3.Solution();
             Day4 day4 = new Day4(); day4.Solution();
             Day5 day5 = new Day5(); day5.Solution();
+            Day6 day6 = new Day6(); day6.Solution();
         }
     }
 
